b62e74763940010e2a5b234e47bda5b1620b13b2
Vincent Battaglia <vincent.battaglia@gmail.com>
2022-08-06 06:20:39 +0200

    Config for mainnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || "";
 const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY || "";
+const MAINNET_PRIVATE_KEY = process.env.MAINNET_PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
@@ -12,6 +13,10 @@ const config: HardhatUserConfig = {
     rinkeby: {
       url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
       accounts: [RINKEBY_PRIVATE_KEY],
+    },
+    mainnet: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [MAINNET_PRIVATE_KEY],
     },
   },
 };
